4acec0968035cc6ec0807df1fe3fa3312d0befd3
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-04-02 19:51:15 +0200

    feat: added signup

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://localhost:27017/securelogin', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var app = express();
 
---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+
+const login= (req, res, next)=>{
+
+};
+const  signup= async(req,res,next)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    const user = new User({
+        username: username
+    });
+    await user.setPassword(password);
+    await user.save().then(result => {
+        res.json({
+            "status":"succes"
+        })
+    }).catch(error => {
+        res.json({
+            'status': "error"
+        })
+    });
+};
+module.exports.signup = signup;
+module.exports.login= login;
